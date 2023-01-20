2f5a238ef253d435e526ee320468b85c4d559524
priyank vaghela <vaghelapriyank142@gmail.com>
2023-01-20 13:35:05 +0530

    Router Config Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './curd/product-details/product-details.component';
+import { ProductFromComponent } from './curd/product-from/product-from.component';
+import { ProductListComponent } from './curd/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'curd',pathMatch:'full'},
+  {path:'curd', loadChildren: ()=>import('./curd/curd.module').then(m=>m.CURDModule)}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/curd/curd-routing.module.ts
+++ src/app/curd/curd-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductFromComponent } from './product-from/product-from.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'product-list',pathMatch:'full'},
+  {path:'product-list',component: ProductListComponent},
+  {path:'create-product',component: ProductFromComponent},
+  {path:'update-product/:productId',component: ProductListComponent},
+  {path:'view-product-details/:productId',component: ProductDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
